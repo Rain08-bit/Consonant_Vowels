4cd2340eb49c1bce17cffc6f212a28a07a19a9d3
Rain08-bit <57347003+Rain08-bit@users.noreply.github.com>
2019-11-11 16:50:28 +0800

    Add files via upload

---
+++ Kho_LeapYear.py
@@ -0,0 +1,7 @@
+year = int(input("Year = "))
+if year%4==0:
+	print(year, " is a leap year")
+else:
+	print(year, " is not a leap year")
+    
+#Leap year happens every 4 years which means a year should be divisible by 4 to be leap year
